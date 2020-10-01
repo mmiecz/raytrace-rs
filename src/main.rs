21c8c390345ef9c59d5ab2dc2bea53d3b5941a21
Michal Mieczkowski <mieczkowski@fastmail.com>
2020-10-01 10:13:29 +0200

    Fix bad merge.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-mod vec;
-
-use image::{ImageBuffer};
+use image::{ImageBuffer, RgbImage, EncodableLayout};
+use std::io::Write;
 
 const WIDTH: u32 = 512;
 const HEIGHT: u32 = 512;
@@ -10,8 +9,10 @@ fn main() {
         let r: f32 = x as f32 / (WIDTH as f32 - 1.0);
         let g: f32 = y as f32 / (HEIGHT as f32 - 1.0);
         let b: f32 = 0.25;
+        let mut stdout = std::io::stdout();
+        stdout.lock();
+        stdout.write(format!("\rScanlines remaining: {}", y).as_bytes());
         image::Rgb([(255.999 * r) as u8, (255.999 * g) as u8, (255.999 * b) as u8])
     });
-    image.save("output.png").unwrap();
-    println!("Hello, world!");
+    image.save("output.png");
 }
