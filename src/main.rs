1ead9236c4c048a0a1c42bb59d20ca4197cf8e4e
Michał Mieczkowski <mieczkowski@fastmail.com>
2020-10-12 22:27:20 +0200

    Nalgebra tests.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 mod types;
 
-use image::{EncodableLayout, ImageBuffer, RgbImage};
 use std::io::Write;
-
-use types::Color;
 
 const WIDTH: u32 = 512;
 const HEIGHT: u32 = 512;
 
-fn main() {
-    let image = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| {
-        let y = HEIGHT - y; // bottom up;
-        let r: f32 = x as f32 / (WIDTH as f32 - 1.0);
-        let g: f32 = y as f32 / (HEIGHT as f32 - 1.0);
-        let b: f32 = 0.25;
-        let mut stdout = std::io::stdout();
-        stdout.lock();
-        stdout.write(format!("\rScanlines remaining: {}", y).as_bytes());
-        let color = Color::new((r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8);
-        image::Rgb(color.as_array())
-    });
-    image.save("output.png");
-}
+fn main() {}
--- src/types.rs
+++ src/types.rs
@@ -1,260 +1,94 @@
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use nalgebra as na;
+use std::ops::Neg;
 
-type Point = Vec3;
-
-pub struct Color {
-    rgb: [u8; 3],
-}
-
-impl Color {
-    pub fn new(r: u8, g: u8, b: u8) -> Self {
-        Color { rgb: [r, g, b] }
-    }
-
-    pub fn as_array(&self) -> [u8; 3] {
-        self.rgb
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub struct Ray {
-    origin: Point,
-    dir: Vec3,
-}
-
-impl Ray {
-    pub fn new(origin: Point, dir: Vec3) -> Ray {
-        Ray { origin, dir }
-    }
-
-    pub fn origin(&self) -> Point {
-        self.origin
-    }
-
-    pub fn direction(&self) -> Vec3 {
-        self.dir
-    }
-
-    pub fn at(&self, t: f32) -> Point {
-        self.origin + self.dir * t
-    }
-}
-
-#[derive(Copy, Clone, Debug)]
-pub struct Vec3([f32; 3]);
-
-impl Vec3 {
-    pub fn new(a: f32, b: f32, c: f32) -> Self {
-        Vec3 { 0: [a, b, c] }
-    }
-    pub fn x(&self) -> f32 {
-        self.0[0]
-    }
-
-    pub fn y(&self) -> f32 {
-        self.0[1]
-    }
-
-    pub fn z(&self) -> f32 {
-        self.0[2]
-    }
-
-    pub fn length(&self) -> f32 {
-        let len_squared = self.0[0] * self.0[0] + self.0[1] * self.0[1] + self.0[2] * self.0[2];
-        len_squared.sqrt()
-    }
-
-    pub fn dot(&self, rhs: Self) -> f32 {
-        self.0[0] * rhs.0[0] + self.0[1] * rhs.0[1] + self.0[2] * rhs.0[2]
-    }
-
-    pub fn cross(&self, rhs: Self) -> Self {
-        Vec3::new(
-            self.0[1] * rhs.0[2] - self.0[2] * rhs.0[1],
-            self.0[2] * rhs.0[0] - self.0[0] * rhs.0[2],
-            self.0[0] * rhs.0[1] - self.0[1] * rhs.0[0],
-        )
-    }
-
-    pub fn unit(&self) -> Self {
-        *self / self.length()
-    }
-}
-
-//TODO: Move this to macro
-// Op<Vec3> for `Scalar`
-// Op<`Scalar`> for Vec3
-impl Sub for Vec3 {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Vec3 {
-            0: [
-                self.0[0] - rhs.0[0],
-                self.0[1] - rhs.0[1],
-                self.0[2] - rhs.0[2],
-            ],
-        }
-    }
-}
-
-impl SubAssign for Vec3 {
-    fn sub_assign(&mut self, rhs: Self) {
-        self.0[0] -= rhs.0[0];
-        self.0[1] -= rhs.0[1];
-        self.0[2] -= rhs.0[2];
-    }
-}
-
-impl Neg for Vec3 {
-    type Output = Self;
-
-    fn neg(self) -> Self::Output {
-        Vec3 {
-            0: [-self.0[0], -self.0[1], -self.0[2]],
-        }
-    }
-}
-
-impl Add for Vec3 {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Vec3 {
-            0: [
-                self.0[0] + rhs.0[0],
-                self.0[1] + rhs.0[1],
-                self.0[2] + rhs.0[2],
-            ],
-        }
-    }
-}
-
-impl AddAssign for Vec3 {
-    fn add_assign(&mut self, rhs: Self) {
-        self.0[0] += rhs.0[0];
-        self.0[1] += rhs.0[1];
-        self.0[2] += rhs.0[2];
-    }
-}
-//TODO: more types than f32
-impl Mul<f32> for Vec3 {
-    type Output = Self;
-
-    fn mul(self, rhs: f32) -> Self::Output {
-        Vec3 {
-            0: [self.0[0] * rhs, self.0[1] * rhs, self.0[2] * rhs],
-        }
-    }
-}
-
-impl MulAssign<f32> for Vec3 {
-    fn mul_assign(&mut self, rhs: f32) {
-        self.0[0] *= rhs;
-        self.0[1] *= rhs;
-        self.0[2] *= rhs;
-    }
-}
-
-impl Div<f32> for Vec3 {
-    type Output = Self;
-
-    fn div(self, rhs: f32) -> Self::Output {
-        Vec3 {
-            0: [self.0[0] / rhs, self.0[1] / rhs, self.0[2] / rhs],
-        }
-    }
-}
-
-impl DivAssign<f32> for Vec3 {
-    fn div_assign(&mut self, rhs: f32) {
-        self.0[0] /= rhs;
-        self.0[1] /= rhs;
-        self.0[2] /= rhs;
-    }
-}
+type Vec3 = na::Vector3<f32>;
+type Mat3 = na::Matrix3<f32>;
+type Point = na::Point3<f32>;
 
 #[cfg(test)]
 mod tests {
-    use super::Vec3;
-
+    use super::*;
     macro_rules! vec3_compare {
-        ($v:expr, $exp:expr) => {
+        ($vec:expr, $exp:expr) => {
             let eps = f32::EPSILON;
-            assert!(($v.x() - $exp[0]).abs() < eps);
-            assert!(($v.y() - $exp[1]).abs() < eps);
-            assert!(($v.z() - $exp[2]).abs() < eps);
+            assert!(($vec.x - $exp[0]).abs() < eps);
+            assert!(($vec.y - $exp[1]).abs() < eps);
+            assert!(($vec.z - $exp[2]).abs() < eps);
         };
     }
-
     #[test]
-    fn test_vec3() {
+    fn vec3_creation() {
         let v = Vec3::new(0.0, 1.0, 2.0);
         vec3_compare!(v, [0.0, 1.0, 2.0]);
     }
 
     #[test]
-    fn test_add() {
+    fn vec3_add() {
+        let v1 = Vec3::new(0.0, 1.0, 2.0);
+        let v2 = Vec3::new(3.0, 4.0, 5.0);
+        let result = v1 + v2;
+        vec3_compare!(result, [3.0, 5.0, 7.0]);
+    }
+
+    #[test]
+    fn vec3_sub() {
+        let v1 = Vec3::new(0.0, 1.0, 2.0);
+        let v2 = Vec3::new(3.0, 4.0, 5.0);
+        let result = v1 - v2;
+        vec3_compare!(result, [-3.0, -3.0, -3.0]);
+    }
+
+    #[test]
+    fn vec3_neg() {
         let v = Vec3::new(0.0, 1.0, 2.0);
-        let v2 = Vec3::new(0.0, 1.0, 2.0);
-        let result = v + v2;
-        vec3_compare!(result, [0.0, 2.0, 4.0]);
+        vec3_compare!(v.neg(), [0.0, -1.0, -2.0]);
     }
 
     #[test]
-    fn test_neg() {
-        let v = Vec3::new(-1.0, 1.0, 0.0);
-        let neg_v = -v;
-        vec3_compare!(neg_v, [1.0, -1.0, 0.0]);
+    fn vec3_scalar_mul() {
+        let scalar = 3.0;
+        let v = Vec3::new(0.0, 1.0, 2.0);
+        let result1 = v * scalar;
+        let result2 = scalar * v;
+        assert_eq!(result1, result2);
+        vec3_compare!(result1, [0.0, 3.0, 6.0]);
     }
 
     #[test]
-    fn test_add_assign() {
-        let mut v = Vec3::new(0.0, 1.0, 2.0);
-        v += Vec3::new(1.0, 1.0, 1.0);
-        vec3_compare!(v, [1.0, 2.0, 3.0]);
+    fn vec3_scalar_div() {
+        let scalar = 3.0;
+        let v = Vec3::new(0.0, 3.0, 6.0);
+        let result1 = v / scalar;
+        vec3_compare!(result1, [0.0, 1.0, 2.0]);
     }
 
     #[test]
-    fn test_mul() {
-        let v = Vec3::new(0.0, 1.0, 5.0);
-        let v = v * 3.0;
-        vec3_compare!(v, [0.0, 3.0, 15.0]);
+    fn vec3_len() {
+        let v = Vec3::new(0.0, 3.0, 6.0);
+        assert_eq!(v.norm(), (3.0_f32.powi(2) + 6.0_f32.powi(2)).sqrt());
     }
 
     #[test]
-    fn test_mul_assign() {
-        let mut v = Vec3::new(0.0, 1.0, 5.0);
-        v *= 3.0;
-        vec3_compare!(v, [0.0, 3.0, 15.0]);
+    fn vec3_norm() {
+        let v = Vec3::new(4.0, 0.0, 0.0);
+        let normalized = v.normalize();
+        assert_eq!(normalized.norm(), 1.0);
+        assert_eq!(v.normalize(), Vec3::new(1.0, 0.0, 0.0));
     }
 
     #[test]
-    fn test_div_assign() {
-        let mut v = Vec3::new(0.0, 1.0, 5.0);
-        v /= 2.0;
-        vec3_compare!(v, [0.0, 0.5, 2.5]);
+    fn vec3_dot() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(2.0, 3.0, 4.0);
+        assert_eq!(v1.dot(&v2), 20.0);
     }
 
-    #[test]
-    fn test_vec3_length() {
-        let vec = Vec3::new(3.0, 4.0, 5.0);
-        assert!((vec.length() - 50.0_f32.sqrt()).abs() < f32::EPSILON);
-    }
-
-    #[test]
-    fn test_vec3_dot() {
-        let vec = Vec3::new(1.0, 3.0, -5.0);
-        let vec2 = Vec3::new(4.0, -2.0, -1.0);
-        let result = vec.dot(vec2);
-        assert!((result - 3.0).abs() < f32::EPSILON);
-    }
-
-    #[test]
-    fn test_vec3_cross() {
-        let vec = Vec3::new(2.0, 3.0, 4.0);
-        let vec2 = Vec3::new(5.0, 6.0, 7.0);
-        let result = vec.cross(vec2);
-        vec3_compare!(result, [-3.0, 6.0, -3.0]);
+    fn vec3_cross() {
+        let v1 = Vec3::new(1.0, 2.0, 3.0);
+        let v2 = Vec3::new(2.0, 3.0, 4.0);
+        let result1 = v1.cross(&v2);
+        let result2 = v2.cross(&v1);
+        assert_eq!(result1, result2.neg());
+        vec3_compare!(result1, [-1.0, 2.0, -1.0]);
     }
 }
